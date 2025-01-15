1c227369377db582148cc944a00d56c495f24671
fatmapostaci <144628832+fatmapostaci@users.noreply.github.com>
2025-01-16 01:45:27 +0300

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,7 +1,6 @@
-import java.util.List;
+import java.util.HashMap;
 
-public abstract class User implements UserActions {
-
+public abstract class User {
 
     private String userName;
     private String password;
@@ -9,19 +8,49 @@ public abstract class User implements UserActions {
 
     UserType userType;
 
-    public User(UserType userType) {
-        this.userType = userType;
-    }
-
     //admin constructor
     public User(String userName, String password, String mailAdress, UserType userType) {
+
         this.userName = userName;
         this.password = password;
         this.mailAdress = mailAdress;
         this.userType = userType;
     }
-    public abstract boolean logout();
-    public abstract boolean login();
+
+    public User() {
+
+    }
+    abstract void logout();
+
+    abstract void login();
+
+    abstract void loggedInMenu();
+
+
+    void returnToMainMenu() {
+        Action.mainMenu();
+    }
+
+    //kullanıcı ismi listede mevcut ise password kontrolü yapar
+    boolean passwordControl() {
+
+        int counter = 0;
+        do {
+            // döngü bitene kadar password kullanıcıdan input olarak alınır
+            System.out.print("---Pasword: ");
+            String password = TryCatch.stringInput();
+            //eğerki password ile input eşleşiyorsa method sonlanır, true döner
+            if (getPassword().equals(password)) {
+                return true;
+            }
+
+            else System.out.print("Hatalı password.Tekrar deneyin : ");
+            counter++;
+        } while (counter < 3);  //3 kez şifre girene kadar döngü devam eder
+
+        //password ile input değer eşleşmiyorsa method false döner
+        return false;
+    }
 
     public String getUserName() {
         return userName;
@@ -47,10 +76,10 @@ public abstract class User implements UserActions {
         this.mailAdress = mailAdress;
     }
 
-
     public UserType getUserType() {
         return userType;
     }
 
-
+    void register() {
+    }
 }
